5539b827f4fc5470b42fc633e88ce1149ac67c18
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2022-06-26 17:02:41 -0300

    feat: button rgb/hex

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const colors = ['green', 'red', 'rgba(133, 122, 200)', '#F15025'];
-const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+
+const btn = document.getElementById('btn');
+const colors = ['red', 'green', 'blue'];
+
+const hexBtn = document.getElementById('hexBtn');
+const colorsHex = ['#FF0000', '#00FF00', '#0000FF'];
+
+const rgbBtn = document.getElementById('rgbBtn');
+const colorsRgb = ['rgb(255, 0, 0)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)'];
 
 function getRandomNumber() {
     return Math.floor(Math.random() * colors.length);
@@ -8,7 +15,26 @@ function getRandomNumber() {
 
 btn.addEventListener('click', function() {
     let randomNumber = getRandomNumber();
-    console.log(randomNumber);
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
+})
+
+function getRandomNumberHex() {
+    return Math.floor(Math.random() * colorsHex.length);
+}
+
+hexBtn.addEventListener('click', () => {
+    let randomNumberHex = getRandomNumberHex();
+    document.body.style.backgroundColor = colorsHex[randomNumberHex];
+    color.textContent = colorsHex[randomNumberHex];
+})
+
+function getRandomNumberRgb() {
+    return Math.floor(Math.random() * colorsRgb.length);
+}
+
+rgbBtn.addEventListener('click', () => {
+    let randomNumberRgb = getRandomNumberRgb();
+    document.body.style.backgroundColor = colorsRgb[randomNumberRgb];
+    color.textContent = colorsRgb[randomNumberRgb];
 })
